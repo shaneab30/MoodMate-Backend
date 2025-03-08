f342a27161594aa228987d3f8f12ff0c83a21a8a
Shane Baskara <shaneardyanto@gmail.com>
2025-03-08 10:26:19 +0700

    added image preprocess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, json, render_template, request
+from flask import Flask, json, render_template, request, jsonify
+from ml_model.ML import predict_image
+from werkzeug.utils import secure_filename
 from flask_restful import Api
 from flask_cors import CORS, cross_origin
 from controller.users_controller import UsersController
@@ -13,20 +15,47 @@ api.add_resource(UsersController, '/users', '/users/register')
 # api.add_resource(PredictController, '/predict', '/predict/<string:userId>')
 # api.add_resource(OrdersController, '/orders', '/orders/<string:orderId>')
 
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+        
+        try:
+            file.save(file_path)
+            label = predict_image(file_path)
+            return jsonify({"message": "File successfully uploaded", "filename": filename, "label": label}), 200
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+
+    return jsonify({"error": "File type not allowed"}), 400
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # print(request.files)
-    print(json.dumps(request.files.to_dict(flat=False), indent=4))
-    if 'file' not in request.files:
-        return "No file part", 400  # Return a 400 Bad Request if file is missing
-
-    f = request.files['file']
-    if f.filename == '':
-        return "No selected file", 400  # Handle empty file selection
-
-    f.save(f.filename)   
-    return "File uploaded successfully"
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    return jsonify({"message": "Prediction successful"}), 200
 
 
 if __name__ == "__main__":
---
+++ ml_model/ML.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+from PIL import Image
+from pickle import load
+import numpy as np
+
+def predict_image(image_path)->str:
+
+    image = Image.open(image_path).convert('L')
+
+    # (width, height) = image.size
+    # # Calculate the minimum dimension to ensure a 1:1 aspect ratio
+    # min_dim = min(width, height)
+    # # Calculate the left and top coordinates to center the crop
+    # left = (width - min_dim) // 2
+    # top = (height - min_dim) // 2
+    # # Crop the image to a 1:1 aspect ratio
+    # image = image.crop((left, top, left + min_dim, top + min_dim))
+    image = image.resize((48, 48))
+    image = np.array(image).flatten()
+    image = image/255.0
+
+    # image.save(f'resized_{image_name}.png')
+    print(image)
+
+    # scale image with standard scaler
+    X_scaled, y_encoded,label_encoder, pca, scaler = load(open("/Users/shaneab/Projects/Fullstack project/backend-skripsi/ml_model/labelencoder_standardscaler_pca_normalizers_dump.pkl", "rb"))
+    image = pca.transform(image)
+    image = scaler.transform(image)
+
+    # machine learning
+    svc_model = load(open("knn_model_standardscaler_grisearch_pca_dump.pkl", "rb"))
+    y = svc_model.predict(image)
+
+    label = label_encoder.inverse_transform(y)
+    # label = label_encoder.inverse_transform([y])
+
+    print(label)
+    return None
